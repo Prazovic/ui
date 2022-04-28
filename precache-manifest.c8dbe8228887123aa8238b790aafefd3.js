f8df3a525d45a625ab15053cf12e3ef15b8555ed
Baliarena <97719937+Baliarena@users.noreply.github.com>
2022-04-29 01:46:28 +0800

    Updates

---
+++ precache-manifest.c8dbe8228887123aa8238b790aafefd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84e0f1e71e2f196c942c51109f17892",
+    "url": "/ui/index.html"
+  },
+  {
+    "revision": "53a6857a56a4348057c1",
+    "url": "/ui/static/css/2.06ebec9a.chunk.css"
+  },
+  {
+    "revision": "fd9e52b95af8613206a6",
+    "url": "/ui/static/css/main.70bbcaee.chunk.css"
+  },
+  {
+    "revision": "53a6857a56a4348057c1",
+    "url": "/ui/static/js/2.920dbd5a.chunk.js"
+  },
+  {
+    "revision": "70fd27d7534104d11b965a74ff67c29f",
+    "url": "/ui/static/js/2.920dbd5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd9e52b95af8613206a6",
+    "url": "/ui/static/js/main.a733748b.chunk.js"
+  },
+  {
+    "revision": "5a2144400fa68ec6661c",
+    "url": "/ui/static/js/runtime-main.15a8801d.js"
+  },
+  {
+    "revision": "2797d55e0f33a408ae543aaa6c30e2f9",
+    "url": "/ui/static/media/logo.2797d55e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.344b42b71772b1239b62e9d95433fcd7.js"
+  "/ui/precache-manifest.c8dbe8228887123aa8238b790aafefd3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
